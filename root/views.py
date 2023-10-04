63261ad1d959ea319cc28372c5bd290363f24a67
mozmmil-ali <mly88207@gmail.com>
2023-10-04 16:35:31 +0200

    update books-finder class

--- root/views.py
+++ root/views.py
@@ -1,5 +1,7 @@
-# import time
+import time
+
 import g4f
+from g4f.Provider import Bard, Bing
 
 # from django.http import  StreamingHttpResponse
 from rest_framework.decorators import api_view
@@ -9,8 +11,6 @@ from rest_framework.views import APIView
 
 from .serializers import MessageSerializer
 
-# from g4f.Provider import   Bard,   Bing
-
 
 class MessageApi(APIView):
     max_try = 50
@@ -19,28 +19,45 @@ class MessageApi(APIView):
 
     serializer_class = MessageSerializer
 
+    # def message_generator(self, message):
+    #     response = "<!DOCTYPE html>"
+    #     while True and self.max_try > 0:
+    #         try:
+    #             response = g4f.ChatCompletion.create(
+    #                 model="gpt-3.5-turbo",
+    #                 messages=[{"role": "user", "content": message}],
+    #                 # stream=True,
+    #                 provider=Bing,
+    #             )
+    #         except Exception as e:
+    #             # print(e)
+    #             self.max_try -= 1
+    #             continue
+    #         # yield from response
+    #         if "<!DOCTYPE html>" not in response:
+    #             return response
+    #             # for message in response:
+    #             #     yield message
+    #             # break
+    #         self.max_try -= 1
+    #     # yield "Network Error"
+    #     return "Network Error!, Please try again."
     def message_generator(self, message):
-        response = "<!DOCTYPE html>"
-        while True and self.max_try > 0:
-            try:
-                response = g4f.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[{"role": "user", "content": message}],
-                    # stream=True,
-                    # provider=Bing
-                )
-            except Exception as e:
-                # print(e)
-                self.max_try -= 1
-                continue
-            # yield from response
-            if "<!DOCTYPE html>" not in response:
-                return response
-                # for message in response:
-                #     yield message
-                # break
-            self.max_try -= 1
-        # yield "Network Error"
+        try:
+            return g4f.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": message}],
+                provider=Bard,
+            )
+        except Exception as e:
+            return g4f.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": message}],
+                provider=Bing,
+            )
+        except:
+            pass
+
         return "Network Error!, Please try again."
 
     def get(self, request, format=None):
@@ -49,12 +66,15 @@ class MessageApi(APIView):
         return Response(usernames)
 
     def post(self, request, format=None):
+        # start = time.time()
         message = request.data.get("message", "")
         response = self.message_generator(message)
         # response = StreamingHttpResponse(
         #     self.message_generator(message), status=200, content_type="text/event-stream"
         # )
         # response["Cache-Control"] = ("no-cache",)
+        # end = time.time()
+        # print(f"time: {end-start}")
         return Response(response)
 
 
